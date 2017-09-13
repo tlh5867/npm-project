6c6416419c02cc371a53c437b6f38bcbd78f2e2d
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-13 12:09:50 -0500

    configure taks to run webpack automatically

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 /*...*/
+
+const $ = require("jquery");
+const data = require("./data");
+const math = require("./math");
+
+
+
+
+
+
 const hello = () =>console.log("hello world from webpack!");
 
-hello()
+hello();
+
+console.log(data.whichSideOfTheForce);
+console.log(data.arnie);
+
+
+$("body")
+    .html("<h1>Hello from jQuery</h1>")
+    .css("background-color","gray")
+;
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.js',
     // where to put the transpiled javascript
     output: {
-        path: path.resolve(__dirname, 'public/jsnpm'),
+        path: path.resolve(__dirname, 'public/js'),
         filename: 'main.js'
     },
 
